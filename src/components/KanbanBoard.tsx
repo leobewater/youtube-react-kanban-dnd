445f234a0fc11f6c99bf0546734c4129bd9ccee1
leobewater <mmbee888@gmail.com>
2023-10-17 07:25:36 -0400

    swap element when drop

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -2,9 +2,15 @@ import { useMemo, useState } from 'react';
 import PlusIcon from '../icons/PlusIcon';
 import { Column, Id } from '../types';
 import ColumnContainer from './ColumnContainer';
-import { DndContext, DragOverlay, DragStartEvent } from '@dnd-kit/core';
-import { SortableContext } from '@dnd-kit/sortable';
+import {
+  DndContext,
+  DragEndEvent,
+  DragOverlay,
+  DragStartEvent,
+} from '@dnd-kit/core';
+import { SortableContext, arrayMove } from '@dnd-kit/sortable';
 import { createPortal } from 'react-dom';
+import { act } from 'react-dom/test-utils';
 
 const KanbanBoard = () => {
   const [columns, setColumns] = useState<Column[]>([]);
@@ -15,7 +21,7 @@ const KanbanBoard = () => {
 
   return (
     <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]">
-      <DndContext onDragStart={onDragStart}>
+      <DndContext onDragStart={onDragStart} onDragEnd={onDragEnd}>
         <div className="m-auto flex gap-4">
           <div className="flex gap-4">
             <SortableContext items={columnsId}>
@@ -75,6 +81,29 @@ const KanbanBoard = () => {
       setActiveColumn(event.active.data.current.column);
       return;
     }
+  }
+
+  function onDragEnd(event: DragEndEvent) {
+    console.log('DRAG END', event);
+    const { active, over } = event;
+
+    if (!over) return;
+
+    const activeColumnId = active.id;
+    const overColumnId = over.id;
+
+    if (activeColumnId === overColumnId) return;
+
+    // swap elements
+    setColumns((columns) => {
+      const activeColumnIndex = columns.findIndex(
+        (col) => col.id === activeColumnId
+      );
+      const overColumnIndex = columns.findIndex(
+        (col) => col.id === overColumnId
+      );
+      return arrayMove(columns, activeColumnIndex, overColumnIndex);
+    });
   }
 };
 
