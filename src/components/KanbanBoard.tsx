68ca107661ac3ada7baf2cddc18eda3e8443751c
leobewater <mmbee888@gmail.com>
2023-10-17 07:09:35 -0400

    wrap columns with DndContext

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import PlusIcon from '../icons/PlusIcon';
-import { Column } from '../types';
+import { Column, Id } from '../types';
 import ColumnContainer from './ColumnContainer';
+import { DndContext } from '@dnd-kit/core';
 
 const KanbanBoard = () => {
   const [columns, setColumns] = useState<Column[]>([]);
@@ -9,25 +10,29 @@ const KanbanBoard = () => {
 
   return (
     <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]">
-      <div className="m-auto flex gap-4">
-        <div className="flex gap-4">
-          {columns.map((col) => (
-            <div key={col.id}>
-              <ColumnContainer column={col} deleteColumn={deleteColumn} />
-            </div>
-          ))}
-        </div>
+      <DndContext>
+        <div className="m-auto flex gap-4">
+          <div className="flex gap-4">
+            {columns.map((col) => (
+              <ColumnContainer
+                key={col.id}
+                column={col}
+                deleteColumn={deleteColumn}
+              />
+            ))}
+          </div>
 
-        <button
-          onClick={() => {
-            createNewColumn();
-          }}
-          className="h-[60px] w-[350px] min-w-[350px] rounded-lg bg-mainBackgroundColor border-2 border-columnBackgroundColor p-4 ring-rose-500 hover:ring-2 flex gap-2"
-        >
-          <PlusIcon />
-          Add Column
-        </button>
-      </div>
+          <button
+            onClick={() => {
+              createNewColumn();
+            }}
+            className="h-[60px] w-[350px] min-w-[350px] rounded-lg bg-mainBackgroundColor border-2 border-columnBackgroundColor p-4 ring-rose-500 hover:ring-2 flex gap-2"
+          >
+            <PlusIcon />
+            Add Column
+          </button>
+        </div>
+      </DndContext>
     </div>
   );
 
