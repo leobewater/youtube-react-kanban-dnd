54051c0f3778e37b71b95aab551ddd0db61234f8
leobewater <mmbee888@gmail.com>
2023-10-17 08:21:58 -0400

    make task draggable

--- src/components/ColumnContainer.tsx
+++ src/components/ColumnContainer.tsx
@@ -1,8 +1,8 @@
-import { useSortable } from '@dnd-kit/sortable';
+import { SortableContext, useSortable } from '@dnd-kit/sortable';
 import TrashIcon from '../icons/TrashIcon';
 import { Column, Id, Task } from '../types';
 import { CSS } from '@dnd-kit/utilities';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import PlusIcon from '../icons/PlusIcon';
 import TaskCard from './TaskCard';
 
@@ -28,6 +28,11 @@ const ColumnContainer = (props: Props) => {
   } = props;
 
   const [editMode, setEditMode] = useState(false);
+
+  // re-calculate task ids when add/remove tasks
+  const tasksIds = useMemo(() => {
+    return tasks.map((task) => task.id);
+  }, [tasks]);
 
   // initial sortable item object
   const {
@@ -110,12 +115,14 @@ const ColumnContainer = (props: Props) => {
       {/* Column task container */}
       <div className="flex flex-grow flex-col gap-4 p-2 overflow-x-hidden overflow-y-auto">
         {tasks.map((task) => (
-          <TaskCard
-            key={task.id}
-            task={task}
-            deleteTask={deleteTask}
-            updateTask={updateTask}
-          />
+          <SortableContext items={tasksIds}>
+            <TaskCard
+              key={task.id}
+              task={task}
+              deleteTask={deleteTask}
+              updateTask={updateTask}
+            />
+          </SortableContext>
         ))}
       </div>
 
--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import TrashIcon from '../icons/TrashIcon';
 import { Id, Task } from '../types';
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 
 interface Props {
   task: Task;
@@ -12,6 +14,28 @@ const TaskCard = ({ task, deleteTask, updateTask }: Props) => {
   const [mouseIsOver, setMouseIsOver] = useState(false);
   const [editMode, setEditMode] = useState(false);
 
+  // initial sortable for task items
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: task.id,
+    data: {
+      type: 'Task',
+      task,
+    },
+    disabled: editMode,
+  });
+
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
+
   const toggleEditMode = () => {
     setEditMode((prev) => !prev);
     setMouseIsOver(false);
@@ -19,7 +43,13 @@ const TaskCard = ({ task, deleteTask, updateTask }: Props) => {
 
   if (editMode) {
     return (
-      <div className="flex bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center text-left rounded-xl hover:ring-2 hover:ring-inset hover:ring-rose-500 cursor-grab relative">
+      <div
+        ref={setNodeRef}
+        style={style}
+        {...attributes}
+        {...listeners}
+        className="flex bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center text-left rounded-xl hover:ring-2 hover:ring-inset hover:ring-rose-500 cursor-grab relative"
+      >
         <textarea
           className="h-[90%] w-full resize-none border-none rounded bg-transparent text-white focus:outline-none"
           value={task.content}
@@ -37,6 +67,10 @@ const TaskCard = ({ task, deleteTask, updateTask }: Props) => {
 
   return (
     <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
       onClick={toggleEditMode}
       className="flex bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center text-left rounded-xl hover:ring-2 hover:ring-inset hover:ring-rose-500 cursor-grab relative task"
       onMouseEnter={() => {
